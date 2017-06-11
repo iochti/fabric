b76e3f7a55f7b1c42779f3bd18e59f74d217259f
Luc Chmielowski <luc.chmielowski@gmail.com>
2017-06-11 11:50:48 +0200

    Modified namerd config & SQL tables

--- db/postgres/iochti.sql
+++ db/postgres/iochti.sql
@@ -1,21 +1,26 @@
-CREATE TYPE units AS ENUM (
-  'celsius',
-  'farenheit',
-  'lux',
-  'kilograms',
-  'file'
+CREATE TABLE IF NOT EXISTS account (
+  id serial PRIMARY KEY,
+  name varchar(40),
+  login varchar(40) UNIQUE,
+  avatar varchar(250),
+  email varchar(100) UNIQUE,
+  created_at date,
+  updated_at date
 );
 
 CREATE TABLE IF NOT EXISTS data_group (
   id serial PRIMARY KEY,
   name varchar(50),
-  unit units
+  unit varchar(10)
 );
 
 CREATE TABLE IF NOT EXISTS thing_group (
   id serial PRIMARY KEY,
+  account_id int REFERENCES account,
   name varchar(50),
-  description varchar(200)
+  description varchar(200),
+  created_at timestamp,
+  updated_at timestamp
 );
 
 CREATE TABLE IF NOT EXISTS data_group_things_group (
@@ -26,19 +31,9 @@ CREATE TABLE IF NOT EXISTS data_group_things_group (
 
 CREATE TABLE IF NOT EXISTS thing (
   id  serial PRIMARY KEY,
-  thing_id integer REFERENCES thing_group (id),
+  group_id integer REFERENCES thing_group (id),
   name varchar(50),
   MAC varchar(17),
   created_at timestamp,
   updated_at timestamp
-);
-
-CREATE TABLE IF NOT EXISTS accounts (
-  id serial PRIMARY KEY,
-  name varchar(40),
-  login varchar(40) UNIQUE,
-  avatar varchar(250),
-  email varchar(100) UNIQUE,
-  created_at date,
-  updated_at date
 );
